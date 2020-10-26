1b04e893ee1e00bd69fcf5702e35dba37a120641
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-26 10:14:52 -0500

    added new stuff

--- Race_Cars/Start_game.py
+++ Race_Cars/Start_game.py
@@ -2,12 +2,11 @@ from menu import *
 from carimage import *
 
 
-
+os.system('clear')
 for i in ["LET'S RACE!!!",race_car_image_1,race_car_image_2]:#Splash screen when game starts
     print(i)
     time.sleep(1.5)
     os.system('clear')
 
 
-    
 start_game()
--- Race_Cars/menu.py
+++ Race_Cars/menu.py
@@ -10,6 +10,13 @@ def start_game():#This runs the game. It is called in the Start_game script and 
     while True:
         try:
             if get_user_input == "YES" or get_user_input == "Y":
+                while True:
+                    try:
+                        new_game = Game(int(input("How many rounds?")))
+                        break
+                    except ValueError:
+                        print("Please enter a valid number of rounds")
+                
                 new_game.race(all_cars)
                 break
             elif get_user_input not in new_game.acceptable_answers:
